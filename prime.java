c0aaefdeb8316a1cd3289cffd5e33088b4172b06
Oggy36cock <22h51a0588@cmrcet.ac.in>
2024-12-11 11:35:28 +0530

    three

--- prime.java
+++ prime.java
@@ -1,7 +1,7 @@
 class prime{
 public static void main(String[] args){
 int i,c=0;
-System.out.println("prime numbers");
+System.out.println("prime isnumbers");
 for(i=1;i<=100;i++){
 for(j=1;j<sqrt(i);j++){
 if(i%j==0)
