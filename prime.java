87b869fbe3647f260e1deaad4fb7464394fb843a
Oggy36cock <22h51a0588@cmrcet.ac.in>
2024-12-11 11:44:11 +0530

    fiveeeeee

--- prime.java
+++ prime.java
@@ -2,17 +2,18 @@ class prime{
 public static void main(String[] args){
 int i,j,c=0;
 System.out.println("prime numbers");
-for(i=1;i<=100;i++){
-for(j=1;j<i/2;j++){
+for(i=2;i<=100;i++){
+    c=0;
+for(j=2;j<=i/2;j++){
 if(i%j==0)
 {c=1;
 break;
 }
-
+}
 if(c==0){
-System.out.print(i);
+System.out.print(i+ " ");
 }
-}
+
 }
 }
 }
