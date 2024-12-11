0e67794bd74973b00cf5ff3f6529720455aaf31a
Oggy36cock <22h51a0588@cmrcet.ac.in>
2024-12-11 11:32:16 +0530

    k

---
+++ prime.java
@@ -0,0 +1,17 @@
+class prime{
+public static void main(String[] args){
+int i,c=0;
+System.out.println("prime numbers");
+for(i=1;i<=100;i++){
+for(j=1;j<sqrt(i);j++){
+if(i%j==0)
+{c=1;
+break;
+}
+}
+if(c==0){
+System.out.print(i);
+}
+}
+}
+}
